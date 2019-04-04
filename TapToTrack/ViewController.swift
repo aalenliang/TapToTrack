da3e3713f33c0969a3ed7887bd67310a9a0d86f7
Alen Liang <simplyaalen@gmail.com>
2019-04-04 22:03:13 +0800

    Initial Commit

---
+++ TapToTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapToTrack
+//
+//  Created by Alen Liang on 2019/4/4.
+//  Copyright © 2019 Alen Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TapToTrackTests/TapToTrackTests.swift
@@ -0,0 +1,34 @@
+//
+//  TapToTrackTests.swift
+//  TapToTrackTests
+//
+//  Created by Alen Liang on 2019/4/4.
+//  Copyright © 2019 Alen Liang. All rights reserved.
+//
+
+import XCTest
+@testable import TapToTrack
+
+class TapToTrackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
